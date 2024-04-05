4314689b5f9c165ca90550c73d9aa496e28ec563
VisShon <vishnu20414@iiitd.ac.in>
2024-04-06 04:12:18 +0530

    performace updates

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import {createRoot} from "react-dom/client"
 import App from "./pages/App"
 import Navbar from "./components/Navbar"
 
-import "./styles/index.css"
+import "./styles/main.min.css"
 import { ReactLenis } from "@studio-freight/react-lenis"
 
 
